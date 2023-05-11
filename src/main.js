362f80061640da8efe9c44d4ac43f6c24083510d
VictorSilva654 <victordejesus654@gmail.com>
2023-05-11 18:37:55 -0300

    Ajustes no Mobile e finalizando o template

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 document.addEventListener('DOMContentLoaded', function(){
     const buttons = document.querySelectorAll('[data-tab-button]');
+    const questions = document.querySelectorAll('[data-faq-question]');
+    const heroPosition = document.querySelector('.hero');
+    const heightHero = heroPosition.clientHeight;
 
     for (let i = 0; i< buttons.length; i++){
         buttons[i].addEventListener('click', function(button){
@@ -11,6 +14,20 @@ document.addEventListener('DOMContentLoaded', function(){
             button.target.classList.add('shows__tabs__button--active');
         })
     }
+
+    for (let i = 0; i < questions.length; i++) {
+        questions[i].addEventListener('click', openCloseAnswer);
+    }
+
+    window.addEventListener('scroll', function(){
+        const actualPosition = window.scrollY;
+
+        if (heightHero > actualPosition) {
+            hideHeader();
+        }else {
+            showHeader();
+        }
+    })
 })
 
 function removeButtonActive(){
@@ -27,4 +44,21 @@ function hideTabs(){
     for (let i =0; i <tabsContainer.length; i++){
         tabsContainer[i].classList.remove('shows__list--active');
     }
+}
+
+function openCloseAnswer(element){
+    const classe = "faq__questions__item--is-open";
+    const elementFather = element.target.parentNode;
+
+    elementFather.classList.toggle(classe);
+}
+
+function hideHeader () {
+    const header = document.querySelector('header');
+    header.classList.add('header--is-hidden');
+}
+
+function showHeader(){ 
+    const header = document.querySelector('header');
+    header.classList.remove('header--is-hidden');
 }
